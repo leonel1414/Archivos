d76acdadef3232e5b4e22acf57f260241fd80212
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-13 20:53:45 -0300

    Modificaciones en lectura de archivos

--- Archivos/Program.cs
+++ Archivos/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Crear archivos TXT y realizar operaciones
+
+using System.IO;
+
+string texto;
+
+try {
+    StreamReader sr = new StreamReader("miArchivo.txt");
+    var linea = sr.ReadLine();
+
+    while (linea != null)
+    {
+        Console.WriteLine(linea);
+        linea = sr.ReadLine();
+    }
+    sr.Close();
+} catch (Exception ex) {
+
+
+}
