4e391c27b657b0aabcd59934f67b721678e2e5cc
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-17 19:10:57 -0300

    Manejo de excepciones

--- Archivos/Program.cs
+++ Archivos/Program.cs
@@ -25,9 +25,11 @@ try {
  * request  = peticion al servidor del API
  * response = gurdamos los datos que obtenemos de la peticion
  */
+
+/*
 using System.Net;
 
-HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://cat-fact.heroku.com/facts");
+HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://pokeapi.co/api/v2/pokemon/ditto");
 
 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
@@ -36,3 +38,31 @@ Stream stream = response.GetResponseStream();
 StreamReader reader = new StreamReader(stream);
 
 var json = reader.ReadToEnd();
+
+Console.WriteLine(json);
+
+*/
+
+
+//MANEJO DE EXCEPCIONES
+//Situaciones inesperada -> no deseada -> "error"
+
+Console.WriteLine($"1 entre 2: {DivisionSegura(1,2)}");
+try {
+    Console.WriteLine($"1 entre 0: {DivisionSegura(1, 0)}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ha ocurrido este Error: {ex.Message}");
+}
+
+
+static double DivisionSegura(double a, double b) {
+
+    if (b == 0) { 
+        throw new DivideByZeroException();
+    
+    }
+    return a / b;
+}
+
